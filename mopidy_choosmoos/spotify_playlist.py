b74a8db621c5105d4056eaa9ad04e5cebae29fc3
Doron Horwitz <doron@milktek.com>
2019-03-09 19:07:25 +0200

    beginnigs of spotify playlist

---
+++ mopidy_choosmoos/spotify_playlist.py
@@ -0,0 +1,34 @@
+import requests
+import time
+from operator import itemgetter
+
+
+class SpotifyPlaylist(object):
+
+    def __init__(self, client_id, client_secret):
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._access_token = None
+        self._access_token_expires_at = None
+
+    def _get_spotify_token(self):
+        if not self._access_token or not self._access_token_expires_at or time.time() > self._access_token_expires_at:
+            self._access_token, expires_in = itemgetter('access_token', 'expires_in')(
+                requests.post('https://auth.mopidy.com/spotify/token', data={
+                    'client_id': self._client_id,
+                    'client_secret': self._client_secret,
+                    'grant_type': 'client_credentials',
+                }).json())
+
+            self._access_token_expires_at = time.time() + expires_in
+
+        return self._access_token
+
+    def get_tracks(self, playlist_id):
+        access_token = self._get_spotify_token()
+        print(access_token)
+        response = requests.get('https://api.spotify.com/v1/playlists/{playlist_id}'.format(playlist_id=playlist_id), headers={
+            'Authorization': 'Bearer {access_token}'.format(access_token=access_token)
+        })
+        return response.json()
+
