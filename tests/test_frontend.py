3de8906ec6d841e29cd8e4b337b47627acda4487
Doron Horwitz <doron@milktek.com>
2020-10-23 13:23:45 +0300

    cleanup tests

--- tests/test_frontend.py
+++ tests/test_frontend.py
@@ -1,55 +1,10 @@
-import os
-from unittest import mock
-from unittest.mock import call
-
 import pykka
-from mopidy import core
-
-from gpiozero import Device
-from gpiozero.pins.mock import MockFactory
-
-
+import os
 from mopidy_choosmoos import Extension
 from mopidy_choosmoos import frontend as frontend_lib
-
-from . import dummy_audio, dummy_backend, dummy_mixer
 from mopidy_choosmoos.web import choosmoos_web_factory
-
-
-dummy_config = {
-    "choosmoos": {
-        "nfc_demo_app_location": None,
-        "next_pin_number": 3,
-        "previous_pin_number": 4,
-        "volume_up_pin_number": 1,
-        "volume_down_pin_number": 2,
-        "play_pause_pin_number": 5,
-    },
-    "spotify": {
-        "username": "alice",
-        "password": "password",
-        "bitrate": 160,
-        "volume_normalization": True,
-        "private_session": False,
-        "timeout": 10,
-        "allow_cache": True,
-        "allow_network": True,
-        "allow_playlists": True,
-        "search_album_count": 20,
-        "search_artist_count": 10,
-        "search_track_count": 50,
-        "toplist_countries": ["GB", "US"],
-        "client_id": "abcd1234",
-        "client_secret": "YWJjZDEyMzQ=",
-    },
-}
-
-
-def dummy_mopidy_core():
-    mixer = dummy_mixer.create_proxy()
-    audio = dummy_audio.create_proxy()
-    backend = dummy_backend.create_proxy(audio=audio)
-    return core.Core.start(audio=audio, mixer=mixer, backends=[backend]).proxy()
+from unittest import mock
+from unittest.mock import call
 
 
 def stop_mopidy_core():
@@ -78,13 +33,8 @@ def test_get_frontend_classes():
     stop_mopidy_core()
 
 
-def test_anything(monkeypatch):
-    def mock_system(val):
-        pass
+def test_anything(frontend):
 
-    monkeypatch.setattr(os, "system", mock_system)
-
-    Device.pin_factory = MockFactory()
-    frontend_lib.ChoosMoosFrontend(dummy_config, dummy_mopidy_core())
+    frontend.on_start()
 
     stop_mopidy_core()
